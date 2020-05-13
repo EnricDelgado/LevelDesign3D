e64182fe22f3b445e9111ca021c6bbc028a963c2
RunningLate <enricdelgadoclaramunt@gmail.com>
2020-05-13 19:49:54 +0200

    fp controller

---
+++ Assets/Resources/_FPS Player/Scripts/Helpers/WaterHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterHelper : MonoBehaviour
+{
+    public enum WaterType { top, underwater }
+    public WaterType type;
+    PlayerController player = null;
+    SurfaceSwimmingMovement swimming = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player = other.GetComponent<PlayerController>();
+        swimming = player.GetSwimmingMovement();
+
+        if (swimming == null) return; //if you didn't get a component then it will be null so don't continue
+        if (type == WaterType.underwater && !swimming.onWaterSurface)
+            swimming.StartSwim();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (swimming == null) return; //if you didn't get a component then it will be null so don't continue
+        if (type == WaterType.top)
+            swimming.WithinWaterTop(true);
+        else
+            swimming.CurrentlyInWater(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (swimming == null) return; //if you didn't get a component then it will be null so don't continue
+        if (type == WaterType.top)
+            swimming.WithinWaterTop(false);
+        if (type == WaterType.underwater)
+            swimming.CurrentlyInWater(false);
+    }
+}
