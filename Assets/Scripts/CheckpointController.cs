6c0a3181a4bf3b1c961c9ace289fa5f11d7a67d2
RunningLate <enricdelgadoclaramunt@gmail.com>
2020-05-20 03:19:50 +0200

    checkpoint implementation with errors

---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerRespawnController>().lastPosition = transform.position;
+        }
+    }
+}
